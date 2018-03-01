21b9efd397b849e073d2c611df17a98ca469da9d
AnLeonovich <novka666@mail.ru>
2018-03-01 15:37:11 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+ var arr = [];
+ var counter=0;
+ var j=0;
+
+ if (array.length < 2) return array.length;
+  
+ for(i = 0; i < array.length; i++){
+    arr[array[i]] = array[i];
+ }
+
+for(i = 0; i < arr.length; i++){
+    if(i in arr){
+      counter++;  
+    } else {
+      if (counter > j){
+         j = counter;
+      }
+      counter = 0;
+   }
+}
+
+return j;
 }
